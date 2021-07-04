da9a584124a52e2a144c24a207a5c7b17352c215
Jörn Schöndube <js45vybi@balin.esa.informatik.tu-darmstadt.de>
2021-07-04 16:54:57 +0200

    experiment with different sample sizes

--- scripts/tpc-h/learn-ensemble.sh
+++ scripts/tpc-h/learn-ensemble.sh
@@ -1,6 +1,6 @@
 python3 maqp.py --generate_ensemble \
     --dataset tpc-h \
-    --samples_per_spn 10000000 \
+    --samples_per_spn 1000 \
     --ensemble_strategy single \
     --hdf_path ../tpc-h/gen_single_light \
     --ensemble_path ../tpc-h/spn_ensembles \
