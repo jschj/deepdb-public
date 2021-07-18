545c5f396cf9a2c6b565721e8012bf52116497bc
Jörn Schöndube <js45vybi@dwalin.esa.informatik.tu-darmstadt.de>
2021-07-18 13:17:41 +0200

    modify eval script for leq queries

--- scripts/tpc-h/eval.sh
+++ scripts/tpc-h/eval.sh
@@ -1,8 +1,8 @@
 python3 maqp.py --evaluate_cardinalities \
     --max_variants 1 \
     --dataset tpc-h \
-    --target_path ../tpc-h/cardinality_estimation/tpc-h.csv \
+    --target_path ../tpc-h/cardinality_estimation/count_queries_leq.csv \
     --ensemble_location ../tpc-h/spn_ensembles/ensemble_single_tpc-h_10000000.pkl \
-    --query_file_location ../tpc-h/queries/count_queries.sql \
-    --ground_truth_file_location ../tpc-h/queries/ground_truth_count.pkl \
+    --query_file_location ../tpc-h/queries/count_queries_leq.sql \
+    --ground_truth_file_location ../tpc-h/queries/count_queries_leq_truth.pkl \
     --ensemble_strategy single
