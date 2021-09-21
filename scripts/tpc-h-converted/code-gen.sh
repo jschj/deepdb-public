2275eafcb2cbfd8a52d6cbf102257f86f85f1018
Jörn Schöndube <js45vybi@balin.esa.informatik.tu-darmstadt.de>
2021-09-21 17:21:38 +0200

    add script to generate cpp code

---
+++ scripts/tpc-h-converted/code-gen.sh
@@ -0,0 +1,4 @@
+python3 maqp.py --dataset tpc-h-converted \
+    --ensemble_location ../tpc-h-converted/spn_ensembles/ensemble_single_tpc-h-converted_1000000.pkl \
+    --ensemble_path ../tpc-h-converted/spn_ensembles/ensemble_single_tpc-h-converted_1000000.pkl \
+    --code_generation
